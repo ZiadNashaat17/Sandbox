965e953cdb4619e235409b931d3acbff16382f18
Ziad Nashaat <ziadnashaat03@gmail.com>
2026-01-06 16:31:57 +0200

    refactor: integrate bad-words and libphonenumber-js for input validation

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
-import { connect } from "mongoose";
+import { Filter } from "bad-words";
+
+import { isValidPhoneNumber } from "libphonenumber-js";
+import { parsePhoneNumber } from "libphonenumber-js/min";
 import app from "./app.js";
 import logger from "./logger.js";
 
@@ -7,13 +10,24 @@ const port = process.env.PORT;
 const filter = new Filter();
 
 try {
-  await connect(DB);
-  console.log("Connected to DB!");
+  console.log(filter.clean("Fuck you"));
+  console.log(filter.isProfane("Fuck you"));
+
+  // const phoneNumber = parsePhoneNumber("+201063520549", "EG");
+  const phoneNumber = "+201063520549";
+  if (isValidPhoneNumber(phoneNumber)) {
+    logger.info("Phone number is valid");
+    //   logger.info(phoneNumber.)
+  } else {
+    logger.error("Invalid phone number");
+  }
 
   app.listen(port, () => {
     console.log(`Sandbox app running on port: ${port}`);
   });
 } catch (error) {
+  console.log("App startup error: ", error);
+  process.exit(1);
   console.log("App startup error: ", error);
   process.exit(1);
 }
