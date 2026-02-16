d7cae5353ccc1cb8432dc12d8ab3f3c6a90c8ba4
Ziad Nashaat <123606129+ZiadNashaat17@users.noreply.github.com>
2026-02-16 16:32:44 +0200

    feat: integrate Twilio for OTP verification and add MongoDB support

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import { config } from "dotenv";
 import express from "express";
 import PinoHttp from "pino-http";
 import logger from "./logger.js";
+import twilio from 'twilio';
+import otpGenerator from 'otp-generator';
 
-config({ path: "./config.env" });
+config({ path: "./.env" });
 
 const app = express();
 
@@ -13,6 +15,31 @@ app.use(PinoHttp({ logger }));
 app.get("/", (req, res) => {
   logger.info("Home page accessed ");
   res.status(200).send("hello world");
+});
+
+const accountSid = process.env.ACC_SID;
+const authToken = process.env.AUTH_TOKEN;
+const serviceSid = process.env.SERVICE_SID;
+const client = twilio(accountSid, authToken);
+
+app.get("/send-verification", async (req, res) => {
+  try {
+    const otp = otpGenerator.generate(6, { upperCaseAlphabets:true, specialChars:false });
+    logger.info({otp})
+
+    const verification = await client.messages.create({
+      body: 'Hello there',
+      from: '+16615901265',
+      to: '+201063520549',
+      channel: 'sms',
+    });
+
+    // logger.info(`Verification sent: ${verification.sid}`);
+    res.status(200).json({ success: true, sid: verification.sid });
+  } catch (error) {
+    logger.error(`Failed to send verification: ${error.message}`);
+    res.status(500).json({ success: false, error: error.message });
+  }
 });
 
 app.get("/error", (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,33 +1,31 @@
 import { Filter } from "bad-words";
 
-import { isValidPhoneNumber } from "libphonenumber-js";
-import { parsePhoneNumber } from "libphonenumber-js/min";
+import { connect } from "mongoose";
 import app from "./app.js";
-import logger from "./logger.js";
 
 const port = process.env.PORT;
-
+const DB_URI = process.env.DB_URI;
 const filter = new Filter();
 
 try {
-  console.log(filter.clean("Fuck you"));
-  console.log(filter.isProfane("Fuck you"));
+  // console.log(filter.clean("Fuck you"));
+  // console.log(filter.isProfane("Fuck you"));
 
   // const phoneNumber = parsePhoneNumber("+201063520549", "EG");
-  const phoneNumber = "+201063520549";
-  if (isValidPhoneNumber(phoneNumber)) {
-    logger.info("Phone number is valid");
-    //   logger.info(phoneNumber.)
-  } else {
-    logger.error("Invalid phone number");
-  }
+  // const phoneNumber = "+201063520549";
+  // if (isValidPhoneNumber(phoneNumber)) {
+  //   logger.info("Phone number is valid");
+  //   //   logger.info(phoneNumber.)
+  // } else {
+  //   logger.error("Invalid phone number");
+  // }
+
+  // await connect(DB_URI);
 
   app.listen(port, () => {
     console.log(`Sandbox app running on port: ${port}`);
   });
 } catch (error) {
-  console.log("App startup error: ", error);
-  process.exit(1);
   console.log("App startup error: ", error);
   process.exit(1);
 }
