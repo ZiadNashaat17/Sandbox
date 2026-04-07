eab91bc5587949853373d83408ab5fdf671464ba
Ziad Nashaat <123606129+ZiadNashaat17@users.noreply.github.com>
2026-04-07 10:30:52 +0200

    feat: add moment dependency and implement basic UTC date handling
    Introduce the moment library to calculate and log the current UTC time and the start of the day. Existing PDF generation code in index.js has been commented out to facilitate testing the new date logic.

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-class BasePdfGenerator {
-  constructor(reportSettings = {}) {
-    this.reportSettings = {
-			type: 'Not Generic',
-			margins: 15,
-			fonts: "FiraCode"
-		};
-    this.type = reportSettings.type || 'Generic';
-    this.margins = this.reportSettings.margins;
-    this.fonts = this.reportSettings.fonts;
-    this.reportType = this.type;
-  }
+// class BasePdfGenerator {
+//   constructor(reportSettings = {}) {
+//     this.reportSettings = {
+// 			type: 'Not Generic',
+// 			margins: 15,
+// 			fonts: "FiraCode"
+// 		};
+//     this.type = reportSettings.type || 'Generic';
+//     this.margins = this.reportSettings.margins;
+//     this.fonts = this.reportSettings.fonts;
+//     this.reportType = this.type;
+//   }
   
-  addNoDataMessage = () => {
-		console.log(this.reportSettings);
-  }
-}
+//   addNoDataMessage = () => {
+// 		console.log(this.reportSettings);
+//   }
+// }
 
-export function createMileageReportSettings(reportSettings = {}) {
-	const baseGenerator = new BasePdfGenerator();
+// export function createMileageReportSettings(reportSettings = {}) {
+// 	const baseGenerator = new BasePdfGenerator();
 
-	return {
-		addNoDataMessage: baseGenerator.addNoDataMessage
-	}
-}
+// 	return {
+// 		addNoDataMessage: baseGenerator.addNoDataMessage
+// 	}
+// }
 
-const reportSettings = createMileageReportSettings()
+// const newBaseGenerator = new BasePdfGenerator();
+// newBaseGenerator.addNoDataMessage()
 
-reportSettings.addNoDataMessage()
+// const reportSettings = createMileageReportSettings()
+
+// reportSettings.addNoDataMessage()
+
+
+import moment from 'moment';
+
+const now = moment().utc();
+const startOfDay = now.clone().startOf('day').toDate();
+
+console.log('moment now: ', now.format());
+console.log(startOfDay);
